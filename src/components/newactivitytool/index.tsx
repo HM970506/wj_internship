8265014c3cace003a5f212eb3777c97b611fdfbc
mimi <62527898+HM970506@users.noreply.github.com>
2023-02-28 16:55:54 +0900

    스티커 추가

--- src/components/newactivitytool/index.tsx
+++ src/components/newactivitytool/index.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useRef, useState } from "react";
-import { Layer, Stage } from "react-konva";
-import { useSelector } from "react-redux";
+import {
+  Layer,
+  Rect,
+  Stage,
+  Transformer,
+  Text,
+  KonvaNodeComponent,
+} from "react-konva";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Background,
   Button,
   ButtonBox,
-  Canvas,
   LoadButton,
   MainButton,
   NewButton,
@@ -13,12 +19,92 @@ import {
   ToolBox,
 } from "./style";
 import TextButton from "./text";
-import { Text } from "react-konva";
+import { KonvaEventObject } from "konva/lib/Node";
+import StickerButton from "./sticker";
 
-const nodeMaker = (value: any, key: number) => {
-  switch (value.type) {
+const Node = ({
+  type,
+  text,
+  shapeProps,
+  isSelected,
+  onSelect,
+  onChange,
+}: {
+  type: any;
+  text: string;
+  shapeProps: any;
+  isSelected: any;
+  onSelect: any;
+  onChange: any;
+}) => {
+  const shapeRef = useRef<KonvaNodeComponent>(null);
+  const trRef = useRef<>(null);
+
+  useEffect(() => {
+    if (isSelected) {
+      trRef.current?.nodes([shapeRef.current]);
+      trRef.current?.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  switch (type) {
     case "text":
-      return <Text text={value.content} key={key} />;
+      return (
+        <>
+          <Text
+            text={text}
+            draggable={true}
+            onClick={onSelect}
+            onTap={onSelect}
+            ref={shapeRef}
+          />
+          {isSelected && <Transformer ref={trRef} />}
+        </>
+      );
+    case "sticker":
+      return (
+        <>
+          <Rect
+            draggable={true}
+            onClick={onSelect}
+            onTap={onSelect}
+            ref={shapeRef}
+            {...shapeProps}
+            onDragEnd={(e) => {
+              onChange({
+                ...shapeProps,
+                x: e.target.x(),
+                y: e.target.y(),
+              });
+            }}
+            onTransformEnd={(e) => {
+              const node = shapeRef.current;
+              const scaleX = node.scaleX();
+              const scaleY = node.scaleY();
+
+              node.scaleX(1);
+              node.scaleY(1);
+              onChange({
+                ...shapeProps,
+                x: node.x(),
+                y: node.y(),
+                // set minimal value
+                // width: Math.max(5, node.width() * scaleX),
+                //  height: Math.max(node.height() * scaleY),
+              });
+            }}
+          />
+          {isSelected && (
+            <Transformer
+              ref={trRef}
+              boundBoxFunc={(oldBox, newBox) => {
+                if (newBox.width < 5 || newBox.height < 5) return oldBox;
+                else return newBox;
+              }}
+            />
+          )}
+        </>
+      );
   }
 };
 
@@ -27,7 +113,10 @@ export default function NewActivityTool() {
   const [subButtonVisible, setSubButtonVisible] = useState<boolean>(false);
   const [activitytools, setActivitytools] = useState<boolean>(false);
   const [canvas, setCanvas] = useState<any[]>([]);
+  const [selecteShape, selectShape] = useState<number | null>(null);
+  const canvasRef = useRef<stage>(null);
   const nodes = useSelector((state: any) => state.node.nodes);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (activitytools) newActivityTool.current?.showModal();
@@ -58,6 +147,17 @@ export default function NewActivityTool() {
   //버튼들은 useCallback 등으로 메모이제이션 해두고,
   //값은 redux로 계속 불러오기
 
+  //배경 누르면 선택 없애기
+  const checkDeselect = (
+    e: KonvaEventObject<TouchEvent> | KonvaEventObject<MouseEvent>
+  ) => {
+    if (e.target == canvasRef.current) selectShape(null);
+  };
+
+  //변화하면, 리덕스 값을 수정합니다
+
+  const shapeChange = (newAttr: any) => {};
+
   return (
     <>
       <Background ref={newActivityTool}>
@@ -68,15 +168,35 @@ export default function NewActivityTool() {
           <TextButton />
           <Button>녹음</Button>
           <Button>사진</Button>
-          <Button>스티커</Button>
+          <StickerButton />
           <Button>도구</Button>
           <Button>test</Button>
         </ButtonBox>
 
-        <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Stage
+          width={window.innerWidth}
+          height={window.innerHeight}
+          onMouseDown={checkDeselect}
+          onTouchStart={checkDeselect}
+          ref={canvasRef}
+        >
           <Layer>
             {Array.isArray(canvas) &&
-              canvas.map((value: any, key: number) => nodeMaker(value, key))}
+              canvas.map((value: any, key: number) => (
+                <Node
+                  key={key}
+                  type={value.type}
+                  shapeProps={value.shapeProps}
+                  text={value.content}
+                  isSelected={key === selecteShape}
+                  onSelect={() => {
+                    selectShape(key);
+                  }}
+                  onChange={(newAttrs: any) => {
+                    shapeChange(newAttrs);
+                  }}
+                />
+              ))}
           </Layer>
         </Stage>
       </Background>
---
+++ src/components/newactivitytool/sticker.tsx
@@ -0,0 +1,5 @@
+import { Button } from "./style";
+
+export default function StickerButton() {
+  return <Button>스티커</Button>;
+}
